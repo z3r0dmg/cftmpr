229cf4d6bee7f951b79e8623f98dabb51a42375d
Stumblef00l <kunalbonucci@gmail.com>
2020-10-09 01:27:41 +0530

    Created package idgen and initial fetch new UID function

---
+++ golang/idgen/idgen.go
@@ -0,0 +1,52 @@
+// Package idgen creates and fetches new random IDs
+package idgen
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// UserIDStruct Struct for new user ID
+type UserIDStruct struct {
+	UID  string `json:"uid"`
+	Used bool   `json:"used"`
+}
+
+// GetNewUID gets a new user ID
+func GetNewUID() string {
+
+	// Connecting to database
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(
+		"mongodb+srv://admin:kunalkushagraekanshi@cluster0.isccg.mongodb.net/cftmpr?retryWrites=true&w=majority",
+	))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Disconnect after query
+	defer client.Disconnect(ctx)
+
+	// Get UserIdStore collection handle
+	userIDStore := client.Database("cftmpr").Collection("UserIdStore")
+
+	// Get One new user ID
+	filter := bson.M{"used": false}
+	var newUser UserIDStruct
+	err = userIDStore.FindOne(context.TODO(), filter).Decode(&newUser)
+
+	// Check for errors
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Return user ID
+	return newUser.UID
+}
